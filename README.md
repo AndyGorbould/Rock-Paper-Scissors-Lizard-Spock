433bd83abdc695befdbe2798ff9b38dc7a1b247e
Andy Gorbould <lost@MacBook-Air.local>
2022-05-19 10:13:10 +0200

    randomizer tested & working

--- README.md
+++ README.md
@@ -11,12 +11,14 @@ _________________________
 - [X] Use this MD to plan the game from the start!
 - [X] Using an array for the RPSLS?
 - [] Select RPSLS from a row of appropriate images
-- [] Button to randomize the "opponent" choice
+- [X] Button to randomize the "opponent" choice
 - [] Using if/else arguments for the winner?
 - [] An element for displaying player/opponent scores after each round
 - [] Button to play next round after results
     this must be disabled until a round is complete
 - [] See if this could be made into an app using Framework7 or alternative (final goal)
+- [] Function A for player win Function B for player lose Function C for tie
+- [] Win func adds playerScore++, Lose opponentScore++, tie no change
 _________________________
 ### Steps taken:
 - [X] Using & modifying MD all the way through the project to keep general oversight
--- script.js
+++ script.js
@@ -7,20 +7,53 @@ const rpsls = [
 ];
 
 // initialize buttons
-const play = document.getElementById("play");
 const resetSelection = document.getElementById("resetSelection");
 const nextRound = document.getElementById("nextRound");
 
+// Score init
+let playerScore = 0;
+let opponentScore = 0;
+let roundNumber = 1;
 
 
+// PLAY button - funcs to run:
+document.getElementById("play").onclick = function() {  
+    opponentChoiceRandomizer()  
+    };  
 
-
-// testing opponent choice randomization
-play.addEventListener("click", opponentChoiceRandomizer());
-
-
-
+// Opponent choice randomizer
 function opponentChoiceRandomizer() {
-    opponentChoice = rpsls[Math.floor(Math.random() * rpsls.length)];
+    let opponentChoice = rpsls[Math.floor(Math.random()*rpsls.length)];
+    // log choice for testing
     console.log(opponentChoice);
 };
+
+
+// Round score handler
+function roundWin() {
+    playerScore = playerScore + 1;
+    alert("You Win!")
+    roundNumber = roundNumber + 1;
+    console.log(`Round ${roundNumber - 1} Player Score: ${playerScore} - Opponent Score: ${opponentScore}`);
+
+}
+function roundLose() {
+    opponentScore = opponentScore + 1;
+    alert("You Lose :(")
+    roundNumber = roundNumber + 1;
+    console.log(`Round ${roundNumber - 1} Player Score: ${playerScore} - Opponent Score: ${opponentScore}`);
+
+}
+function roundTie() {
+    alert("YOU BOTH LOSE!")
+    roundNumber = roundNumber + 1;
+    console.log(`Round ${roundNumber - 1} Player Score: ${playerScore} - Opponent Score: ${opponentScore}`);
+}
+
+
+
+
+// TEST button - funcs to run:
+document.getElementById("testButton").onclick = function() {  
+    roundLose()  
+    };  
